eb08e5a901541d2ff208797d76c9ca4eefd31f7c
Stanislau Latushko <staslatgttt@gmail.com>
2016-10-05 22:36:42 +0000

    add comments in sync.c

--- src/sync.c
+++ src/sync.c
@@ -11,10 +11,15 @@
 int main(int argc, char* argv[]){
   int counter=0;
   int num=0;
+
+  //создание нескольких потоков с частной для каждого потока переменной num
   #pragma omp parallel private(num)
   {
+    //каждый поток узнает свой номер
     num=omp_get_thread_num();
+    //в цикле проверяем, не оказалось ли значение counter равно номеру потока
     while(1){
+      //если оказалось равно, ты поток сообщает об этом, увеличивает counter на 1 и прекращает работу
       if(num==counter){
         printf("I'm %d\n", num);
         counter+=1;
