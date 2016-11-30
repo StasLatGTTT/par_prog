bd2c0a96ea69620bcb2ff52831ebd3b83f48a6a2
Stanislau Latushko <staslatgttt@gmail.com>
2016-11-30 21:08:17 +0000

    add variables for measurement

--- src/lab2/var_1g.c
+++ src/lab2/var_1g.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <omp.h>
+#include <time.h>
 
 #define ISIZE 1000
 #define JSIZE 1000
@@ -8,19 +10,27 @@
 int main(int argc, char **argv)
 {
 	double a[ISIZE][JSIZE];
-	int i, j;
+	int i, j, N = 1;
 	FILE *ff;
+	double start, end, T1, Tp, E, S;
+
+	//consistent computation
 	for (i = 0; i < ISIZE; i++){
 	for (j = 0; j < JSIZE; j++){
 		a[i][j] = 10 * i +j;
 	}
 	}
+
+	start = clock();
 	for (i = 1; i < ISIZE; i++){
 	for (j = 3; j < JSIZE - 1; j++){
 		a[i][j] = sin(0.00001 * a[i - 1][j - 3]);
 	}
 	}
-	ff = fopen("result.txt","w");
+	end = clock();
+	T1 = end - start;
+
+	ff = fopen("result_cons.txt","w");
 	for(i = 0; i < ISIZE; i++){
 		for (j = 0; j < JSIZE; j++){
 			fprintf(ff,"%f ",a[i][j]);
@@ -28,4 +38,34 @@ int main(int argc, char **argv)
 		fprintf(ff,"\n");
 	}
 	fclose(ff);
+
+	//parallel computation
+	for (i = 0; i < ISIZE; i++){
+	for (j = 0; j < JSIZE; j++){
+		a[i][j] = 10 * i +j;
+	}
+	}
+
+	start = clock();
+	for (i = 1; i < ISIZE; i++){
+	for (j = 3; j < JSIZE - 1; j++){
+		a[i][j] = sin(0.00001 * a[i - 1][j - 3]);
+	}
+	}
+	end = clock();
+	Tp = end - start;
+
+	ff = fopen("result_par.txt","w");
+	for(i = 0; i < ISIZE; i++){
+		for (j = 0; j < JSIZE; j++){
+			fprintf(ff,"%f ",a[i][j]);
+		}
+		fprintf(ff,"\n");
+	}
+	fclose(ff);
+
+	S = T1 / Tp;
+	E = S / N;
+	printf("%d with 1 thread\n%d with %d threads\n", T1, Tp, N);
+	printf("S = %d\t E = %d", S, E);
 }
