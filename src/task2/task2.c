abf3e11222eb009372e57f7a9c8d49340d2bd3f3
Stanislau Latushko <staslatgttt@gmail.com>
2016-12-12 23:21:13 +0000

    add parameters initialization

--- src/task2/task2.c
+++ src/task2/task2.c
@@ -5,11 +5,26 @@
 
 #define EPS 0.01
 
-int main(int argc, char* argv){
-	int param = 100, N = 1;
-	double h = 0.1, x_min = -10.0, x_max = 10.0, err = 1;
+int main(int argc, char* argv[]){
+	long int N = 1;
+	double x_min = -10.0, x_max = 10.0, y_left, y_right;
+	double err = 1, h = 1, temp, param = 100.0;
 	double *a, *b, *c, *f, *x, *y, *y_next, *y_der;
 
+	if(argc != 2){
+		printf("Invalid argument number\n");
+		return -1;
+	}
+	param = atof(argv[1]);
+	temp = 1.0 / ((x_max - x_min) * sqrt(param));
+	while(h > temp) {
+		N *= 10;
+		h = (x_max - x_min) / N;
+	}
+	N++;
+	y_left = sqrt(2);
+	y_right = y_left;
+	printf("param = %lf\ttemp = %lf\th = %lf\tN = %d\n", param, temp, h, N);
 
 	a = (double*) malloc(N * sizeof(double));
 	b = (double*) malloc(N * sizeof(double));
