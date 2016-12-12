434cd7e1562b89bbd29c09d222e9fa1d988a6bc8
Stanislau Latushko <staslatgttt@gmail.com>
2016-12-12 22:23:11 +0000

    additional vars and minor style corrections

--- src/task2/task2.c
+++ src/task2/task2.c
@@ -3,19 +3,22 @@
 #include <math.h>
 #include <omp.h>
 
+#define EPS 0.01
+
 int main(int argc, char* argv){
 	int param = 100, N = 1;
-	double h = 0.1, x_min = -10.0, x_max = 10.0;
+	double h = 0.1, x_min = -10.0, x_max = 10.0, err = 1;
 	double *a, *b, *c, *f, *x, *y, *y_next, *y_der;
 
-	a = (double*) malloc((N + 1) * sizeof(double));
-	b = (double*) malloc((N + 1) * sizeof(double));
-	c = (double*) malloc((N + 1) * sizeof(double));
-	f = (double*) malloc((N + 1) * sizeof(double));
-	x = (double*) malloc((N + 1) * sizeof(double));
-	y = (double*) malloc((N + 1) * sizeof(double));
-	y_next = (double*) malloc((N + 1) * sizeof(double));
-	y_der = (double*) malloc((N + 1) * sizeof(double));
+
+	a = (double*) malloc(N * sizeof(double));
+	b = (double*) malloc(N * sizeof(double));
+	c = (double*) malloc(N * sizeof(double));
+	f = (double*) malloc(N * sizeof(double));
+	x = (double*) malloc(N * sizeof(double));
+	y = (double*) malloc(N * sizeof(double));
+	y_next = (double*) malloc(N * sizeof(double));
+	y_der = (double*) malloc(N * sizeof(double));
 
 	free(a);
 	free(b);
